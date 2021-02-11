786a8ff746dc245a7798a2a69417aabf15b3a501
Afonso Costa <afonsir17@gmail.com>
2021-02-11 12:02:16 -0300

    Adding first query

---
+++ schema-query/project/index.js
@@ -0,0 +1,24 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    ola: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    ola() {
+      return 'Bom dia!'
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server is running in ${url}`);
+});
