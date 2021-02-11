dfe3c5ee843974c70e8602ba26e2e0e265da1446
Afonso Costa <afonsir17@gmail.com>
2021-02-11 12:55:12 -0300

    Defining user type

--- schema-query/project/index.js
+++ schema-query/project/index.js
@@ -3,9 +3,19 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
   scalar Date
 
+  type User {
+    id: ID
+    name: String!
+    email: String!
+    age: Int
+    salary: Float
+    vip: Boolean
+  }
+
   type Query {
     hello: String
     currentTime: Date
+    loggedUser: User
   }
 `
 
@@ -16,6 +26,16 @@ const resolvers = {
     },
     currentTime() {
       return new Date;
+    },
+    loggedUser(){
+      return {
+        id: 1,
+        name: 'Ana da Web',
+        email: 'anadaweb@email.com',
+        age: 23,
+        salary: 1234.56,
+        vip: true
+      }
     }
   }
 };
