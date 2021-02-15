076ee1fe12970cf297e8607139365c1de18aee17
Afonso Costa <afonsir17@gmail.com>
2021-02-15 12:22:50 -0300

    Return user's status (as an enum)

--- schema-query/project/data/db.js
+++ schema-query/project/data/db.js
@@ -3,18 +3,21 @@ const users = [
     id: 1,
     name: 'Jim Halpert',
     email: 'jim.halpert@email.com',
+    status: 'ACTIVE',
     age: 33
   },
   {
     id: 2,
     name: 'Pam Beesly',
     email: 'pam.beesly@email.com',
+    status: 'INACTIVE',
     age: 30
   },
   {
     id: 3,
     name: 'Michael Scott',
     email: 'michael.scott@email.com',
+    status: 'BLOCKED',
     age: 40
   }
 ]
