da35bf8dee2215a7bebf7e7b776cef22a2dba775
AfonSir <afonsir17@gmail.com>
2021-03-02 18:27:52 -0300

    Fix: search profile by profile_id

--- schema-query/project/resolvers/User.js
+++ schema-query/project/resolvers/User.js
@@ -5,7 +5,7 @@ module.exports = {
     return parent.custom_salary;
   },
   profile(parent) {
-    const selectedProfiles = profiles.filter(profile => profile.id === parent.id);
+    const selectedProfiles = profiles.filter(profile => profile.id === parent.profile_id);
     return selectedProfiles ? selectedProfiles[0] : null;
   }
 };
